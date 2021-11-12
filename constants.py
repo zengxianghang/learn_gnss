77c377fb54bf2c7e9c106868bf0cc829b9c41153
Xianghang Zeng <zengxianghang@qq.com>
2021-11-12 19:42:50 +0800

    Add settings parameters

---
+++ constants.py
@@ -0,0 +1,10 @@
+
+OPTIONS = { 'speed_light': 2.99792458e8, \
+            'spp_iteration': 10, \
+            'min_ele': 10, \
+            'gravitational_constant': 3.9860050e14,\
+            'semi_major_axis': 6378136.55,\
+            'eccentricity': 8.1819190842622e-2, \
+            'rotation_rate': 7.2921151467e-5, \
+            # 'GC': enable GPS and BDS, 'GER': enable GPS, GAL, and GLO
+            'enable_sys': 'G' }
